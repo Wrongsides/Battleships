b55237dc71af0770cf0c4ad14f5543494d192c87
Wrongsides <johnbyrne.1984@gmail.com>
2016-01-18 18:31:18 +0000

    Sorted out the project structure.

--- tests/TestController.py
+++ tests/TestController.py
@@ -1,5 +1,5 @@
-from battleships_case.Controller import Controller
-from battleships_case.GameBoard import GameBoard
+from battleships.Controller import Controller
+from battleships.GameBoard import GameBoard
 import unittest
 
 
